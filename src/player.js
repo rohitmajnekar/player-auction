e54200b8c54597c5b1857dc8fb4fc85103efe593
rohitmajnekar <56642942+rohitmajnekar@users.noreply.github.com>
2024-05-15 02:28:31 +0530

    minor changes

--- src/player.js
+++ src/player.js
@@ -48,52 +48,55 @@ const PlayerCard = ({ player, teams, set_sold_player, setTeamsData, sale_price, 
   return (
     <div key={player.Name} className="player-card">
        {player.sold && <Confetti gravity={1} />}
-      <div className="player-photo">
-        <img src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/2.png" alt={player.Name} />
-      </div>
-      <div className="player-info">
-        <h1>{player.Name}</h1>
-        <p>Age: {player.Age}</p>
-        <p>Role: {player.Style}</p>
-        <p>Batting Style: {player.battingStyle}</p>
-        <p>Bowling Style: {player.bowlingStyle}</p>
-        <p>Country: India</p>
-        <p>Base Price: {player.Price}</p>
-        <h1 className='bidding-price'>Bidding Price: {sale_price}</h1>
-        {/* Add more relevant information */}
-      </div>
-      {player.sold?(
-        <div>
-          <div className='sold-image'></div>
-          <div className='sold-information'>
-            <h1 style={{padding: '25px'}}> 
-              Sold To
-            </h1>
-            <div style={{fontWeight: 'bold', fontSize: 'x-large'}}>
-              <img style={{alignSelf: 'end'}} src={player.team_logo} alt="Team Logo" className="team-logo card-logo" />
-              <p>
-                {player.team_name}
-              </p>
+       <div className='card-title'>
+         APL 2024
+       </div>
+        <div className="player-photo">
+          <img src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/2.png" alt={player.Name} />
+        </div>
+        <div className="player-info">
+          <h1>{player.Name}</h1>
+          <p>Age: {player.Age}</p>
+          <p>Role: {player.Style}</p>
+          <p>Batting Style: {player.battingStyle}</p>
+          <p>Bowling Style: {player.bowlingStyle}</p>
+          <p>Country: India</p>
+          <p>Base Price: {player.Price}</p>
+          <h1 className='bidding-price'>Bidding Price: {sale_price}</h1>
+          {/* Add more relevant information */}
+        </div>
+        {player.sold?(
+          <div>
+            <div className='sold-image'></div>
+            <div className='sold-information'>
+              <h1 style={{padding: '25px'}}> 
+                Sold To
+              </h1>
+              <div style={{fontWeight: 'bold', fontSize: 'x-large'}}>
+                <img style={{alignSelf: 'end'}} src={player.team_logo} alt="Team Logo" className="team-logo card-logo" />
+                <p>
+                  {player.team_name}
+                </p>
+              </div>
+                {/* <img src={player.team.src}></img> */}
             </div>
-              {/* <img src={player.team.src}></img> */}
           </div>
-        </div>
-      ):(
-        <div>
-          <div className="sale-button button-8" style={{backgroundColor: isEnterPressed? "green":""}} onClick={() => setShowTeams(!showTeams)}>
-            Sale
+        ):(
+          <div>
+            <div className="sale-button button-8" style={{backgroundColor: isEnterPressed? "green":""}} onClick={() => setShowTeams(!showTeams)}>
+              Sale
+            </div>
+            <div className={`team-overlay ${showTeams ? 'show' : ''}`}>
+              <ul className="team-list">
+                {teams.map((team, index) => (
+                  <li key={index} onClick={() => handleTeamSelect(team)}>
+                    {team.name}
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
-          <div className={`team-overlay ${showTeams ? 'show' : ''}`}>
-            <ul className="team-list">
-              {teams.map((team, index) => (
-                <li key={index} onClick={() => handleTeamSelect(team)}>
-                  {team.name}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      )}
+        )}
     </div>
   );
 };
